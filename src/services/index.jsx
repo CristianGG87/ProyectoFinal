fda8494f9728f437a5afb56e5b2122f698252835
CristianGG87 <cristiangg8748@gmail.com>
2023-09-06 19:26:44 +0200

    Registro usuario funcionando

--- src/services/index.jsx
+++ src/services/index.jsx
@@ -6,15 +6,24 @@ export const getAllNewsService = async () => {
   }
   return json.data;
 };
-
 export const getSingleNewsService = async (id) => {
   const response = await fetch(`http://localhost:8000/news/${id}`);
-
   const json = await response.json();
-
   if (!response.ok) {
     throw new Error(json.message);
   }
-
   return json.data.news;
+};
+export const registerUserService = async ({ userName, email, password }) => {
+  const response = await fetch(`http://localhost:8000/users/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userName, email, password }),
+  });
+  const json = await response.json();
+  if (!response.ok) {
+    throw new Error(json.message);
+  }
 };
