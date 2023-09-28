71a5d4909be88bd7566c70deaeef0f6851d7f9e7
Alejandro Brid <alejandrojbrid@gmail.com>
2023-09-28 21:01:03 +0200

    avance

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,33 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { Auth } from "./Auth";
-import SearchBar from "./SearchBar";
-import "./Header.css";
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Auth } from './Auth';
+import SearchBar from './SearchBar';
+import './Header.css';
 export const Header = ({ showAllNews, keywordNews }) => {
-  const [altTop, setAtTop] = useState(true);
-  useEffect(() => {
-    const handler = () => setAtTop(document.scrollingElement.scrollTop === 0);
-    window.addEventListener("scroll", handler);
-    return () => window.removeEventListener("scroll", handler);
-  }, []);
-  return (
-    <header id="header" className={altTop ? "top" : "not-top"}>
-      <Link to="/" className="header-link" onClick={showAllNews}>
-        <div className="logo-container">
-          <img
-            src="../public/news_128x128-32_22252.ico"
-            className="logo"
-            alt="Logo"
-          />
-          <h1> Hack a Boss News</h1>
-        </div>
-      </Link>
-      <SearchBar keywordNews={keywordNews} />
-      <nav className="login">
-        <Auth />
-      </nav>
-    </header>
-  );
+    const [altTop, setAtTop] = useState(true);
+    useEffect(() => {
+        const handler = () =>
+            setAtTop(document.scrollingElement.scrollTop === 0);
+        window.addEventListener('scroll', handler);
+        return () => window.removeEventListener('scroll', handler);
+    }, []);
+    return (
+        <header id="header" className={altTop ? 'top' : 'not-top'}>
+            <Link to="/" className="header-link" onClick={showAllNews}>
+                <div className="logo-container">
+                    <img
+                        src="../public/news_128x128-32_22252.ico"
+                        className="logo"
+                        alt="Logo"
+                    />
+                    <h1> Hack a Boss News</h1>
+                </div>
+            </Link>
+            <SearchBar keywordNews={keywordNews} />
+            <nav className="login">
+                <Auth />
+            </nav>
+        </header>
+    );
 };
 export default Header;
